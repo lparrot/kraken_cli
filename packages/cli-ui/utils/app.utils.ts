952b6185a7a3890b742c79bc34b2a632cd309de2
lauparr <laurent.parrot78@gmail.com>
2023-06-09 13:17:23 +0200

    chore: corrections scripts npm

--- packages/cli-ui/utils/app.utils.ts
+++
@@ -1,5 +0,0 @@
-import {get_project_paths} from "@kraken/cli/src/utils/folders";
-
-export function getProjectPaths() {
-  return get_project_paths(process.env.NODE_ENV === 'development' ? process.env.NUXT_DEV_PROJECT_PATH : process.cwd())
-}
