3e648e59c8ba04ca7a96be678317766f53b5bdfc
laurent.parrot <laurent.parrot78@gmail.com>
2023-08-23 11:12:10 +0200

    feat: ajout de la page de création de timer

--- packages/devtools/pages/generator/timer.vue
+++ packages/devtools/pages/generator/timer.vue
@@ -1,11 +1,63 @@
 <script lang="ts" setup>
+import { convertPathToPackage } from '~/utils/java.utils'
+
 definePageMeta({
   middleware: ['security']
 })
+
+interface Form {
+  cwd: string
+}
+
+const $state = useStateStore()
+const $api = useApiStore()
+const $loader = useAppLoader()
+
+const form = ref<Partial<Form>>({})
+const show = ref({
+  fileselector_cwd: false
+})
+
+const selected_package = computed(() => convertPathToPackage(form.value.cwd!))
+
+function init() {
+  form.value = {}
+}
+
+async function submit() {
+  $loader.start()
+  try {
+
+  } finally {
+    $loader.stop()
+  }
+}
+
+init()
 </script>
 
 <template>
-  Timer
+  <FileSelector v-model="form.cwd" v-model:show="show.fileselector_cwd" :root="$state.paths?.server_java_path"/>
+
+  <UContainer>
+    <VeeForm :initial-values="form" class="space-y-4" validate-on-mount @submit="submit">
+      <UButton :color="form.cwd == null ? 'blue' : 'green'" block @click="show.fileselector_cwd = true">
+        <div v-if="form.cwd == null">Selectionner un package</div>
+        <div v-else>Modifier le package</div>
+      </UButton>
+
+      <template v-if="form.cwd != null">
+        <div class="flex items-center gap-1.5">
+          <div class="font-semibold">Package selectionné :</div>
+          <UIcon class="text-yellow-400" name="i-ic-folder"/>
+          <div>{{ selected_package }}</div>
+        </div>
+
+        <hr/>
+      </template>
+
+    </VeeForm>
+  </UContainer>
 </template>
 
 <style scoped>
