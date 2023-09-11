09fa741aae665f8a865960f63654ad744e1791eb
laurent.parrot <laurent.parrot78@gmail.com>
2023-09-11 08:25:03 +0200

    fix: correction template generation page referentiel

--- packages/devtools/components/DynamicModal.vue
+++ packages/devtools/components/DynamicModal.vue
@@ -22,6 +22,7 @@ async function okAction() {
     if (result !== false) {
       confirming.value = false
     }
+    promptModel.value = null
   } finally {
     submitting.value = false
   }
@@ -31,35 +32,37 @@ async function okAction() {
 <template>
   <Teleport v-if="confirming" :to="params.attachTo">
     <UModal v-model="confirming">
-      <UCard ref="card">
-        <template #header>
-          <div class="flex items-center justify-between">
-            <div class="font-semibold">{{ params.title }}</div>
-            <UButton class="-my-1" color="gray" icon="i-heroicons-x-mark-20-solid" variant="ghost"
-                     @click="confirming = false"/>
-          </div>
-        </template>
-
-        <div class="space-y-2">
-          <template v-if="params.prompt != null">
-            <UFormGroup :label="params.message" name="prompt">
-              <UInput v-model="promptModel" autofocus/>
-            </UFormGroup>
+      <VeeForm @submit="okAction">
+        <UCard ref="card">
+          <template #header>
+            <div class="flex items-center justify-between">
+              <div class="font-semibold">{{ params.title }}</div>
+              <UButton class="-my-1" color="gray" icon="i-heroicons-x-mark-20-solid" variant="ghost"
+                       @click="confirming = false"/>
+            </div>
           </template>
 
-          <div v-else>{{ params.message }}</div>
-        </div>
+          <div class="space-y-2">
+            <template v-if="params.prompt != null">
+              <UFormGroup :label="params.message" name="prompt">
+                <UInput v-model="promptModel" autofocus/>
+              </UFormGroup>
+            </template>
 
-        <template #footer>
-          <div class="flex justify-end space-x-2">
-            <UButton :color="params.buttons?.cancel?.color" @click="cancelAction">Annuler</UButton>
-            <UButton :color="params.buttons?.ok?.color" :disabled="checkIsOkDisabled" :loading="submitting"
-                     variant="solid" @click="okAction">
-              {{ params.label }}
-            </UButton>
+            <div v-else>{{ params.message }}</div>
           </div>
-        </template>
-      </UCard>
+
+          <template #footer>
+            <div class="flex justify-end space-x-2">
+              <UButton :color="params.buttons?.cancel?.color" @click="cancelAction">Annuler</UButton>
+              <UButton :color="params.buttons?.ok?.color" :disabled="checkIsOkDisabled" :loading="submitting"
+                       type="submit" variant="solid">
+                {{ params.label }}
+              </UButton>
+            </div>
+          </template>
+        </UCard>
+      </VeeForm>
     </UModal>
   </Teleport>
 </template>
